3f98aeb0898abbf799203c62ebfd1c2429176823
Trevor Dorn <dorntrevor7@gmail.com>
2020-04-03 08:39:13 -0700

    working online and offline

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-//index.html route.
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
-
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
