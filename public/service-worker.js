1c270a976e28626cceadcfd726ec7f0749073950
Trevor Dorn <dorntrevor7@gmail.com>
2020-04-03 09:01:39 -0700

    playing with manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,8 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/index.js",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/icons/icon-512x512.png",
+    "/manifest.json"
   ];
   
   const CACHE_NAME = "static-cache-v2";
